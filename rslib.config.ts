0606e9f7f39fa1376e09a387d6c07e432c758c80
Sovea\SoveaZhang <harleyhhh423@gmail.com>
2025-09-09 03:00:49 +0800

    Initial commit

---
+++ rslib.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      syntax: 'es2022',
+      bundle: false,
+      dts: true,
+      output: {
+        distPath: {
+          root: './esm',
+        }
+      }
+    },
+  ],
+});
